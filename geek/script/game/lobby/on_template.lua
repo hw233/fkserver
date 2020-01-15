64446e1a2ff97c5bbd3b181d8c621f68d85efc6e
tingo <xxxx@xxx.com>
2020-01-15 11:46:34 +0800

    修改 桌子模板创建

--- geek/script/game/lobby/on_template.lua
+++ geek/script/game/lobby/on_template.lua
@@ -140,6 +140,8 @@ function on_cs_edit_table_template(msg,guid)
         [enum.OPERATION_MODIFY] = on_cs_modify_table_template,
     }
 
+    dump(msg)
+
     local template = msg.template
     if not template then
         send2client_pb(guid,"S2C_EDIT_TABLE_TEMPLATE",{
