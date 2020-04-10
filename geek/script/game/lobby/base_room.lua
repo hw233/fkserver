7721dc822db35c7d0e5ab34a7fa72bb3486ea96d
tingo <xxxx@xxx.com>
2020-04-10 18:54:27 +0800

    修改 去掉多余代码

--- geek/script/game/lobby/base_room.lua
+++ geek/script/game/lobby/base_room.lua
@@ -46,23 +46,6 @@ local function check_conf(conf)
 	assert(conf.room_cfg)
 end
 
-local function find_default_lobby()
-	local services = channel.list()
-    for sid,_ in pairs(services) do
-        local id = sid:match("service%.(%d+)")
-        if id then
-            id = tonumber(id)
-            local conf = serviceconf[id]
-            if conf and (conf.name == nameservice.TNGAME or conf.type == nameservice.TIDGAME) then
-                local gameconf = conf.conf
-                if gameconf and gameconf.first_game_type and gameconf.first_game_type == 1 then
-                    return tonumber(sid:match("service%.(%d+)"))
-                end
-            end
-        end
-    end
-end
-
 function base_room:new()
 	local o = {}
 	setmetatable(o,{__index = base_room,})
