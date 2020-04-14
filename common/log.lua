52a00198ea0d01fb6bf83c6bf00b50e08bb26797
tingo <xxxx@xxx.com>
2020-04-14 11:55:47 +0800

    修改 1、日志加入dump 2、日志文件输出

--- common/log.lua
+++ common/log.lua
@@ -1,9 +1,12 @@
 
 local skynet = require "skynet"
+require "functions"
 
 local print = print
-
+local SERVICE_NAME = SERVICE_NAME
+local logd = ".logd"
 local debuglayer = 3
+
 local function debuginfo()
     return debug.getinfo(debuglayer)
 end
@@ -14,49 +17,125 @@ local function strtime()
     return string.format("[%s.%03d]",os.date("%Y-%m-%d %H:%M:%S",math.floor(time)),ms)
 end
 
+local function do_log_out(s)
+    skynet.send(logd, "lua","do_log",SERVICE_NAME,s)
+
+    print(s)
+end
+
 local log = {}
 
 function log.info(fmt, ...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"INFO",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
 
-    print(s)
+    do_log_out(s)
 end
 
 function log.trace(fmt, ...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"TRACE",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
-    print(s)
+    do_log_out(s)
 end
 
 function log.debug(fmt, ...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"DEBUG",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
-    print(s)
+    do_log_out(s)
 end
 
 function log.assert(fmt,...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"ASSERT",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
-    print(s)
+    do_log_out(s)
 end
 
 function log.warning(fmt, ...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"WARNING",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
-    print("\27[33m"..s.."\27[0m")
+    do_log_out("\27[33m"..s.."\27[0m")
 end
 
 function log.exception(fmt, ...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"EXCEPTION",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
-    print("\27[31m"..s.."\27[0m")
+    do_log_out("\27[31m"..s.."\27[0m")
 end
 
 function log.error(fmt, ...)
     local d = debuginfo()
     local s = string.format("%s %-8s"..fmt,strtime(),"ERROR",...) .. string.format("(%s:%d)",d.short_src,d.currentline)
-    print("\27[31m"..s.."\27[0m")
+    do_log_out("\27[31m"..s.."\27[0m")
 end
+
+local function log_dump(value,description,nesting)
+    if type(nesting) ~= "number" then nesting = 4 end
+
+    local lookupTable = {}
+    local result = {}
+
+    local function dump_value_(v)
+        if type(v) == "string" then
+            v = "\"" .. v .. "\""
+        end
+        return tostring(v)
+    end
+    
+    local function dump_(value, desciption, indent, nest, keylen)
+        desciption = desciption or "<var>"
+        local spc = ""
+        if type(keylen) == "number" then
+            spc = string.rep(" ", keylen - string.len(dump_value_(desciption)))
+        end
+        if type(value) ~= "table" then
+            result[#result +1 ] = string.format("%s%s%s = %s", indent, dump_value_(desciption), spc, dump_value_(value))
+        elseif lookupTable[tostring(value)] then
+            result[#result +1 ] = string.format("%s%s%s = *REF*", indent, dump_value_(desciption), spc)
+        else
+            lookupTable[tostring(value)] = true
+            if nest > nesting then
+                result[#result +1 ] = string.format("%s%s = *MAX NESTING*", indent, dump_value_(desciption))
+            else
+                result[#result +1 ] = string.format("%s%s = {", indent, dump_value_(desciption))
+                local indent2 = indent.."    "
+                local keys = {}
+                local keylen = 0
+                local values = {}
+                for k, v in pairs(value) do
+                    keys[#keys + 1] = k
+                    local vk = dump_value_(k)
+                    local vkl = string.len(vk)
+                    if vkl > keylen then keylen = vkl end
+                    values[k] = v
+                end
+                table.sort(keys, function(a, b)
+                    if type(a) == "number" and type(b) == "number" then
+                        return a < b
+                    else
+                        return tostring(a) < tostring(b)
+                    end
+                end)
+                for i, k in ipairs(keys) do
+                    dump_(values[k], k, indent2, nest + 1, keylen)
+                end
+                result[#result +1] = string.format("%s}", indent)
+            end
+        end
+    end
+    dump_(value, desciption, "- ", 1)
+
+    return table.concat(result,"\n")
+end
+
+function log.dump(value,description,nesting)
+    local str = log_dump(value,desciption,nesting)
+    local d = debuginfo()
+    local s = string.format("%s %-8s",strtime(),"DUMP") .. string.format("(%s:%d)",d.short_src,d.currentline)
+    do_log_out(s .. "\n" .. str)
+end
+
+skynet.init(function()
+	logd = skynet.uniqueservice("service.logd")
+end)
 
 return log
--- common/service/logd.lua
+++ common/service/logd.lua
@@ -1,11 +1,40 @@
 local skynet = require "skynetproto"
 
+local log_name_files = {}
+
+-- os.execute("mkdir ./log")
+
 local CMD = {}
 
-function CMD.do_log(...)
-    
+local function log_file(filename)
+    log_name_files[filename] = log_name_files[filename] or io.open(filename,"a+")
+    return log_name_files[filename]
 end
 
-skynet.start(function() 
+function CMD.do_log(servicename,log)
+    local filename = string.format("./log/%s_%s.log",servicename,os.date("%Y-%m-%d"))
+    local file = log_file(filename)
+    file:write(log.."\n")
+    file:flush()
+end
 
+skynet.start(function()
+	skynet.dispatch("lua", function (_, _, cmd, ...)
+		local f = CMD[cmd]
+		if f then
+			skynet.retpack(f(...))
+		else
+			log.error("unknown cmd:"..cmd)
+			skynet.retpack(nil)
+		end
+	end)
+
+	require "skynet.manager"
+	local handle = skynet.localname ".logd"
+	if handle then
+		skynet.exit()
+		return handle
+	end
+
+	skynet.register ".logd"
 end)
