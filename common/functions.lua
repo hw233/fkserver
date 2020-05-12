b876538e062d1d069ccdd94a45b4ff6e8c3de144
xxxx <xxxx@xxxx.com>
2020-05-12 16:35:22 +0800

    修改 table.series函数返回空值不使用自身value值

--- common/functions.lua
+++ common/functions.lua
@@ -430,7 +430,7 @@ table.count = table.nums
 function table.series(tb,fn)
     local s = {}
     for k,v in pairs(tb or {}) do
-        v = fn and fn(v,k) or v
+        v = fn and fn(v,k)
         if v then table.insert(s,v) end
     end
 
