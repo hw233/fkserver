e44c46365f5528477ac8e4dfd33d11e598aa7865
tingo <tingo@xxx.com>
2020-07-06 19:00:03 +0800

    修改 支掉rsa支持

--- geek/script/gate/service/utild.lua
+++ geek/script/gate/service/utild.lua
@@ -1,13 +1,13 @@
 local skynet = require "skynet"
 local httpc = require "http.httpc"
-local rsa = require "rsa"
+-- local rsa = require "rsa"
 local datacenter = require "skynet.datacenter"
 local mmdb = require "mmdb"
 local log = require "log"
 require "functions"
 
-local rsa_pri_key = rsa.gen_key()
-local rsa_public_key = rsa.public_key(rsa_pri_key)
+-- local rsa_pri_key = rsa.gen_key()
+-- local rsa_public_key = rsa.public_key(rsa_pri_key)
 local geodb
 local geodb_path = "./geek/script/data/GeoLite2-City.mmdb"
 
@@ -18,11 +18,11 @@ function CMD.request_sms(guid_or_session)
 end
 
 function CMD.get_rsa_public_key()
-    return rsa.parse(rsa_public_key).n
+    -- return rsa.parse(rsa_public_key).n
 end
 
 function CMD.rsa_decrypt(s)
-    return rsa.decrypt(rsa_pri_key,s)
+    -- return rsa.decrypt(rsa_pri_key,s)
 end
 
 function CMD.request_sms(who)
