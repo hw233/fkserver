f46a0b4d111da0568a37ae67685de0a37ac194a2
tingo <tingo@xxx.com>
2020-07-28 20:01:13 +0800

    修改 club_role优化

--- geek/script/game/club/club_role.lua
+++ geek/script/game/club/club_role.lua
@@ -6,10 +6,22 @@ local reddb = redisopt.default
 local club_role = {}
 
 setmetatable(club_role,{
-    __index = function(t,club_id)
-        local rs = reddb:hgetall("club:role:"..tostring(club_id))
-        local roles = table.map(rs,function(role,guid) return tonumber(guid),tonumber(role) end)
-        return roles
+    __index = function(t_club,club_id)
+        local m = setmetatable({club_id = club_id},{
+            __index = function(t,guid)
+                if not guid then
+                    local rs = reddb:hgetall("club:role:"..tostring(t.club_id))
+                    local roles = table.map(rs,function(srole,sguid) return tonumber(sguid),tonumber(srole) end)
+                    return roles
+                end
+
+                local role = reddb:hget(string.format("club:role:%s",t.club_id),guid)
+                return tonumber(role)
+            end
+        })
+
+        t_club[club_id] = m
+        return m
     end
 })
 
