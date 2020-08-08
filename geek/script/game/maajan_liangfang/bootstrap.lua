550c37d7544199d3a71d36e4a9f2c1375e3ec386
tingo <tingo@xxx.com>
2020-08-08 23:26:59 +0800

    修改 去掉两房bootstrap pb加载文件

--- geek/script/game/maajan_liangfang/bootstrap.lua
+++ geek/script/game/maajan_liangfang/bootstrap.lua
@@ -5,7 +5,6 @@ local enum = require "pb_enums"
 require "game.maajan_liangfang.register"
 
 local function boot(conf)
-    pb.loadfile("gamingcity/pb/common_msg_maajan.proto")
     local room = require "game.maajan_liangfang.maajan_room"
     
     room:init(conf, 4, enum.GAME_READY_MODE_ALL)
