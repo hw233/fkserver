7412734a9698045e1fe17e7d9858dcc43d1f5ec2
tingo <tingo@xxx.com>
2020-08-18 15:47:29 +0800

    修改 联盟同级加入出bug

--- geek/script/game/club/club_conf.lua
+++ geek/script/game/club/club_conf.lua
@@ -7,13 +7,9 @@ local club_conf = {}
 setmetatable(club_conf,{
     __index = function(t,club_id)
         local conf = reddb:hgetall(string.format("club:conf:%d",club_id))
-        if not conf or table.nums(conf) == 0 then
-            return nil
-        end
 
         conf = redismetadata.club.conf:decode(conf)
 
-        -- t[club_id] = conf
         return conf
     end,
 })
