c93cdbf9beb36a91f6b6a3a01ae40d1fa0c47d3c
tingo <tingo@xxx.com>
2020-09-16 17:17:33 +0800

    修复 redis管理脚本:转移club成员数据

--- manage/redis_data_move.py
+++ manage/redis_data_move.py
@@ -15,6 +15,9 @@ for k in club_mem_keys:
     c.zadd("club:zmember:{}".format(cid),
         dict(zip(mems,roles))
         )
+    t = c.hget("club:info:{}".format(cid),"type")
+    for m in mems:
+        c.sadd("player:club:{}:{}".format(m.decode("utf-8"),t.decode("utf-8")),cid)
     
 partner_mem_keys = c.keys("club:partner:member:[0-9]*:[0-9]*")
 for k in partner_mem_keys:
@@ -28,3 +31,5 @@ for k in partner_mem_keys:
     c.zadd("club:partner:zmember:{}:{}".format(cid,pid),
         dict(zip(mems,roles))
         )
+    for m in mems:
+        c.hset("club:member:partner:{}".format(cid),m.decode("utf-8"),pid)
