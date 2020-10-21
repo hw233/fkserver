f44653dbd0bd300cff84923b98b0f8ea59a3192b
tingo <tingo@xxx.com>
2020-10-21 13:58:15 +0800

    修改 sql处理特殊字符

--- geek/script/dbopt.lua
+++ geek/script/dbopt.lua
@@ -1,22 +1,46 @@
 require "functions"
 local log = require "log"
-
 local skynet = require "skynet"
+local json = require "json"
+
+local table = table
+local string = string
 
 local mysqld = ".mysqld"
 
-local function table2sql(tb)
-	local strtb = {}
-	for k,v in pairs(tb) do
-		local t = type(v)
-		if t == "number" or t == "boolean" then
-			table.insert(strtb,k.."="..v)
-		elseif t == "string" then
-			table.insert(strtb,k.."='"..v.."'")
-		elseif t == "table" then
-			table.insert(strtb,k.."="..table2sql(v))
-		end
+local function escape(str)
+	if type(str) ~= "string" then
+		return str
 	end
+
+	return string.gsub(str,"[%%'\"]",function(s)
+		return (s == "%" and "%" or "\\")..s
+	end)
+end
+
+local function uniform_sql_quota(sql)
+	sql = string.trim(sql)
+	local lastchar = string.sub(sql,#sql)
+	return lastchar == ";" and sql or sql .. ";"
+end
+
+local function uniform_sql_field_name(name)
+	return string.format("`%s`",name)
+end
+
+local function table2sql(fields)
+	local strtb = table.series(fields,function(v,k)
+		assert(type(k) == "string")
+		local t = type(v)
+		k = uniform_sql_field_name(k)
+		if t == "string" then
+			return string.format("`%s`='%s'",k,escape(v))
+		end
+		if t == "table" then
+			return string.format("`%s`='%s'",k,json.encode(v))
+		end
+		return string.format("`%s`=%s",k,v)
+	end)
 
 	return table.concat(strtb,",")
 end
@@ -24,7 +48,36 @@ end
 local db = {}
 
 function db:query(sql,...)
-	return skynet.call(mysqld,"lua","query",self.name,sql,...)
+	local args = table.series({...},escape)
+	sql = string.format(sql,table.unpack(args))
+	sql = uniform_sql_quota(sql)
+	return skynet.call(mysqld,"lua","query",self.name,sql)
+end
+
+local function fomrat_batch_sql(s)
+	if type(s) == "table" then
+		if not s[2] then
+			return uniform_sql_quota(s[1])
+		end
+
+		local vargs = table.series(s[2],escape)
+		local sql = string.format(s[1],table.unpack(vargs))
+		sql = uniform_sql_quota(sql)
+		return sql
+	end
+
+	local s = uniform_sql_quota(s)
+	return s
+end
+
+function db:batchquery(sqls,...)
+	if type(sqls) == "table" then
+		sqls = table.series(sqls,fomrat_batch_sql)
+		local sql = table.concat(sqls,"")
+		return skynet.call(mysqld,"lua","query",self.name,sql)
+	end
+
+	return self:query(sqls,...)
 end
 
 function db:execute(sql,tb)
@@ -37,7 +90,19 @@ function db:begin_trans()
 end
 
 function db:do_trans(transid,sql,...)
-	return skynet.call(mysqld,"lua","do_transaction",self.name,transid,sql,...)
+	local args = table.series({...},escape)
+	sql = string.format(sql,table.unpack(args))
+	sql = uniform_sql_quota(sql)
+	return skynet.call(mysqld,"lua","do_transaction",self.name,transid,sql)
+end
+
+function db:do_batchtrans(transid,sqls,...)
+	if type(sqls) == "table" then
+		sqls = table.series(sqls,fomrat_batch_sql)
+		local sql = table.concat(sqls,"")
+		return skynet.call(mysqld,"lua","do_transaction",self.name,transid,sql)
+	end
+	return self:do_trans(transid,sqls,...)
 end
 
 function db:rollback_trans(transid)
@@ -64,6 +129,11 @@ end
 
 function mysql.close(nameorid)
 	skynet.call(mysqld,"lua","close",nameorid)
+end
+
+mysql.escapefield = escape
+mysql.escapefieldname = function(s)
+	return string.format('`%s`',s)
 end
 
 skynet.init(function()
