baf34bb3d68cf44c15b263961f2df91bf76ebdd3
tingo <tingo@xxx.com>
2020-10-30 11:09:30 +0800

    修改 贵州两房胡牌提示

--- geek/script/game/maajan_liangfang/on_maajan.lua
+++ geek/script/game/maajan_liangfang/on_maajan.lua
@@ -142,3 +142,17 @@ function on_cs_do_action(msg,guid)
 		tb:lockcall(function() tb:on_cs_do_action(player, msg) end)
 	end
 end
+
+function on_cs_get_ting_tiles_info(msg,guid)
+	log.info ("test .................. on_cs_get_ting_tiles_info,guid:%s",guid)
+	local player = base_players[guid]
+	if not player then
+		log.error("on_cs_get_ting_tiles_info no player,guid:%s",guid)
+		return
+	end
+
+	local tb = g_room:find_table_by_player(player)
+	if tb then
+		tb:lockcall(function() tb:on_cs_get_ting_tiles_info(player, msg) end)
+	end
+end
