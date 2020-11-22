366d4dba78ef60cf3dfd701c9ba6e4ffcbf9de23
tingo <tingo@xxx.com>
2020-11-22 21:40:44 +0800

    修改 游戏开始后,不取消延迟踢人

--- geek/script/game/lobby/base_table.lua
+++ geek/script/game/lobby/base_table.lua
@@ -2023,7 +2023,7 @@ function base_table:start(player_count)
 	player_count = player_count or table.nums(self.players)
 	log.info("base_table:start %s,%s",self.chair_count,player_count)
 	self:cancel_delay_dismiss()
-	self:cancel_all_delay_kickout()
+	-- self:cancel_all_delay_kickout()
 
 	if not self:is_round_gaming() then
 		self:on_process_start(player_count)
