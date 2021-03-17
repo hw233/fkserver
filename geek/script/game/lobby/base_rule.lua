99f3e3edc27dfc14a8654ba4d5d9be60133090b6
tingo <tingo@xxx.com>
2021-03-17 13:26:20 +0800

    修改 准备时间默认值改为索引0

--- geek/script/game/lobby/base_rule.lua
+++ geek/script/game/lobby/base_rule.lua
@@ -45,7 +45,7 @@ function base_rule.is_owner_start_game(rule)
 end
 
 function base_rule.ready_timeout(rule)
-	local timeout_opt = rule.play and rule.play.ready_timeout_option or 1
+	local timeout_opt = rule.play and rule.play.ready_timeout_option or 0
 	local private_conf = g_room.conf.private_conf
 	return private_conf and private_conf.ready_timeout_option and
 			private_conf.ready_timeout_option[timeout_opt + 1] or
