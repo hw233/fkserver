50409d2db3928d10adb91e956583b9790f07d2dc
tingo <tingo@xxx.com>
2021-05-17 13:40:16 +0800

    修改 net_func send2client

--- geek/script/game/net_func.lua
+++ geek/script/game/net_func.lua
@@ -1,10 +1,12 @@
 local netguidopt = require "netguidopt"
 
 
-function send2client_pb(player, msgname, msg)
+function send2client(player, msgname, msg)
 	local guid = type(player) == "table" and player.guid or player
 	netguidopt.send(guid,msgname,msg)
 end
+
+send2client_pb = send2client
 
 function broadcast2client(guids,msgname,msg)
 	local uniform_guids = table.series(guids,function(p)
