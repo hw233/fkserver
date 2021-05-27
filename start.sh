16dddf6326500e263fca12deb811c71ff4b6f1d4
tingo <tingo@xxx.com>
2021-05-28 00:44:14 +0800

    修改 start.sh killall

--- start.sh
+++ start.sh
@@ -7,12 +7,7 @@ then
 	echo "Warning:应输入cluster id ......"
 fi
 
-pids=$(pgrep skynet)
-if [  "$pids" != ""  ]
-then
-	echo "kill" $pids
-	kill $pids
-fi
+killall skynet
 
 nohup $ROOT/skynet $ROOT/geek/boot.lua  &
 echo "启动成功!"
