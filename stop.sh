ccde61826efcfae7f784be5dfd18ccfb253ce367
tingo <tingo@xxx.com>
2021-05-28 01:17:56 +0800

    修改 stop.sh tail -f nohup.out

--- stop.sh
+++ stop.sh
@@ -2,15 +2,12 @@ echo "kill $(pgrep skynet)";
 
 killall -TERM skynet;
 
-for i in {1..20}; do
-	killall -0 skynet
-	if [ $? -gt 0 ]; then
-		echo "停止成功!";
-		exit;
-	fi
-	echo "等待 $(pgrep skynet) 退出!";
-	sleep 1;
-done
+killall -0 skynet
+if [ $? -gt 0 ]; then
+	echo "停止成功!";
+	exit;
+fi
+tail -f ./nohup.out
 
 echo "等待超时!"
 
