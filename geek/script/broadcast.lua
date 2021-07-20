564a72be0390d1cae793c345ee94829f94a15553
tingo <tingo@xxx.com>
2021-07-20 11:26:36 +0800

    修改 多服务广播

--- geek/script/broadcast.lua
+++ geek/script/broadcast.lua
@@ -1,31 +1,41 @@
 local skynet = require "skynet"
 
+local math = math
+local random = math.random
+
 local broadcast = {}
 
-local broadcastd = ".broadcastd"
+local broadcastd
+local sender
+
+local function choose_sender()
+	sender = sender or skynet.call(broadcastd,"lua","SENDER")
+	assert(sender and type(sender) == "table")
+	return sender[random(1,#sender)]
+end
 
 function broadcast.broadcast2partial(guids,msgname,msg)
-	skynet.send(broadcastd,"lua","broadcast2partial",guids,msgname,msg)
+	skynet.send(choose_sender(),"lua","broadcast2partial",guids,msgname,msg)
 end
 
 function broadcast.broadcast2club(club,msgname,msg)
-	skynet.send(broadcastd,"lua","broadcast2club",club,msgname,msg)
+	skynet.send(choose_sender(),"lua","broadcast2club",club,msgname,msg)
 end
 
 function broadcast.broadcast2online(msgname,msg)
-	skynet.send(broadcastd,"lua","broadcast2online",msgname,msg)
+	skynet.send(choose_sender(),"lua","broadcast2online",msgname,msg)
 end
 
 function broadcast.broadcast2not_gaming(club,msgname,msg)
-	skynet.send(broadcastd,"lua","broadcast2not_gaming",club,msgname,msg)
+	skynet.send(choose_sender(),"lua","broadcast2not_gaming",club,msgname,msg)
 end
 
 function broadcast.broadcast2club_not_gaming(club,msgname,msg)
-	skynet.send(broadcastd,"lua","broadcast2club_not_gaming",club,msgname,msg)
+	skynet.send(choose_sender(),"lua","broadcast2club_not_gaming",club,msgname,msg)
 end
 
-skynet.start(function()
-	broadcastd = skynet.uniqueservice("service.broadcastd")
+skynet.init(function()
+	broadcastd = skynet.uniqueservice("broadcastd")
 end)
 
 return broadcast
---
+++ geek/script/service/broadcastsender.lua
@@ -0,0 +1,74 @@
+local skynet = require "skynet"
+local log = require "log"
+local club_member = require "game.club.club_member"
+local onlineguid = require "netguidopt"
+local channel = require "channel"
+local allonlineguid = require "allonlineguid"
+
+local table = table
+local string = string
+local tinsert = table.insert
+local strfmt = string.format
+
+local broadcastd = ...
+
+local CMD = {}
+
+local function broadcast2guids(guids,msgname,msg,fn)
+	local guid_session = table.map(guids,function(guid)
+		local session = onlineguid[guid]
+		if session and (not fn or fn(session)) then
+			return guid,session.gate
+		end
+	end)
+
+	local gateguids = {}
+    for guid,gate in pairs(guid_session) do
+		gateguids[gate] = gateguids[gate] or {}
+		tinsert(gateguids[gate],guid)
+    end
+
+    for gate,guids in pairs(gateguids) do
+        channel.publish("gate."..gate,"client",guids,"broadcast",msgname,msg)
+    end
+end
+
+function CMD.broadcast2club(club,msgname,msg)
+	local guids = club_member[club]
+	guids = table.series(guids,function(_,guid) return tonumber(guid) end)
+	broadcast2guids(guids,msgname,msg or {})
+end
+
+function CMD.broadcast2online(msgname,msg)
+	local all = allonlineguid["*"]
+	local guids = table.keys(all)
+	broadcast2guids(guids,msgname,msg or {})
+end
+
+function CMD.broadcast2partial(guids,msgname,msg)
+	broadcast2guids(guids,msgname,msg or {})
+end
+
+function CMD.broadcast2not_gaming(guids,msgname,msg)
+	broadcast2guids(guids,msgname,msg or {},function(s) 
+		return not s.table and not s.chair
+	end)
+end
+
+function CMD.broadcast2club_not_gaming(club,msgname,msg)
+	local guids = club_member[club]
+	guids = table.series(guids,function(_,guid) return tonumber(guid) end)
+	CMD.broadcast2not_gaming(guids,msgname,msg)
+end
+
+skynet.start(function()
+	skynet.dispatch("lua", function (_, _, cmd, ...)
+		local f = CMD[cmd]
+		if f then
+			skynet.retpack(f(...))
+		else
+			log.error("unknown cmd:"..cmd)
+			skynet.retpack(nil)
+		end
+	end)
+end)
