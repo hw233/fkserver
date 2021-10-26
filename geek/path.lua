3285bd18350e42c760c9d6580e220a5f8cadab84
tingo <tingo@xxx.com>
2021-10-26 12:54:26 +0800

    修改 搜索目录加入根目录,以便引用fix目录

--- geek/path.lua
+++ geek/path.lua
@@ -1,6 +1,6 @@
 root = "./"
 luaservice = root.."common/?.lua;"..root.."geek/script/?.lua;"..root.."common/service/?.lua;"..root.."geek/script/service/?.lua;"..root.."service/?.lua;"
 lualoader = root.."common/loader.lua"
-lua_path = root.."common/?.lua;"..root.."lualib/?.lua;"..root.."lualib/?/init.lua;"..root.."geek/script/?.lua;"..root.."geek/?.lua"
+lua_path = "./?.lua;"..root.."common/?.lua;"..root.."lualib/?.lua;"..root.."lualib/?/init.lua;"..root.."geek/script/?.lua;"..root.."geek/?.lua"
 lua_cpath = root.."common/luaclib/?.so;"..root .. "luaclib/?.so;"..root .. "geek/luaclib/?.so"
-snax = root.."common/?.lua;"..root.."geek/script/?.lua;"..root.."geek/?.lua"
+snax = "./?.lua;"..root.."common/?.lua;"..root.."geek/script/?.lua;"..root.."geek/?.lua"
