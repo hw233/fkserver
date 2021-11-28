a445d4282071f7928d3a59298d81f33c113bd6dd
tingo <tingo@xxx.com>
2021-11-29 06:37:36 +0800

    修改 club_team_template二级缓存

--- geek/script/game/club/club_team_template.lua
+++ geek/script/game/club/club_team_template.lua
@@ -1,16 +1,19 @@
 local redisopt = require "redisopt"
 require "functions"
 
+local string = string
+
 local reddb = redisopt.default
 
-
-local meta_func = function(_,club_id)
-    local m = setmetatable({club_id = club_id},{
-        __index = function(t,guid)
-            local ttids = reddb:smembers(string.format("club:team:template:%s:%s",t.club_id,guid))
+local meta_func = function(t,club_id)
+    local m = setmetatable({},{
+        __index = function(tb,guid)
+            local ttids = reddb:smembers(string.format("club:team:template:%s:%s",club_id,guid))
             if not ttids or table.nums(ttids) == 0 then
                 return {}
             end
+
+            tb[guid] = ttids
 
             return ttids
         end
@@ -28,7 +31,7 @@ local club_team_template = setmetatable({},{
 local timer_task = require "timer_task"
 
 local function guard()
-    club_member_partner = setmetatable({},{
+    club_team_template = setmetatable({},{
         __index = meta_func
     })
 end 
