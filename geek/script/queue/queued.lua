49038552fd2772563c44396336031dbe17737f3c
tingo <tingo@xxx.com>
2022-05-11 09:42:56 +0800

    修改 queued login

--- geek/script/queue/queued.lua
+++ geek/script/queue/queued.lua
@@ -94,62 +94,32 @@ function CMD.P(...)
 end
 
 function CMD.Login(guid,gate)
-	log.info("Login %s %s",guid,gate)
-	local s = rawget(sessions,guid)
-	if s then
-		local old_gate = s.gate
-		if old_gate and old_gate ~= gate then
-			channel.call("gate."..tostring(old_gate),"lua","kickout",guid)
-		end
+	local l = queues[guid]
+	return l(function()
+		log.info("Login %s %s",guid,gate)
+		local s = rawget(sessions,guid)
+		if s then
+			local old_gate = s.gate
+			if old_gate and old_gate ~= gate then
+				channel.call("gate."..tostring(old_gate),"lua","kickout",guid)
+			end
 
-		local all_lobby = g_common.all_game_server(1)
-		if s.server and not all_lobby[s.server] then
-			local _,reconnect = s:lockcall(function()
-				-- Quit already
-				if not rawget(sessions,guid) then
-					log.warning("Login reconnecting double check got nil session,Quit already,%s",guid)
-					return false
-				end
-
+			local all_lobby = g_common.all_game_server(1)
+			if s.server and not all_lobby[s.server] then
 				s.gate = gate
 				reddb:hset(strfmt("player:online:guid:%d",guid),"gate",gate)
-				local l = queues[guid]
-				return l(channel.call,"game."..s.server,"msg","LS_LoginNotify",guid,true,gate)
-			end)
-
-			if reconnect then
-				return enum.ERROR_NONE,true,s.server
+				local _,reconnect =  channel.call("game."..s.server,"msg","LS_LoginNotify",guid,true,gate)
+				if reconnect then
+					return enum.ERROR_NONE,true,s.server
+				end
 			end
 		end
-	end
 
-	local server = g_common.lobby_id(guid)
-	local s = sessions[guid]
-	return s:lockcall(function()
-		local result
-		local l = queues[guid]
-		-- double check
-		if not rawget(sessions,guid) then
-			s = sessions[guid]
-			return s:lockcall(function()
-				reddb:hmset(strfmt("player:online:guid:%d",guid),{
-					gate = gate
-				})
-
-				result = l(channel.call,"game."..server,"msg","LS_LoginNotify",guid,false,gate)
-				if result == enum.ERROR_NONE then
-					s.gate = gate
-					s.server = server
-				end
-				return result,false,server
-			end)
-		end
-
-		result = l(channel.call,"game."..server,"msg","LS_LoginNotify",guid,false,gate)
+		local server = g_common.lobby_id(guid)
+		s = sessions[guid]
+		local result = channel.call("game."..server,"msg","LS_LoginNotify",guid,false,gate)
 		if result == enum.ERROR_NONE then
-			reddb:hmset(strfmt("player:online:guid:%d",guid),{
-				gate = gate
-			})
+			reddb:hset(strfmt("player:online:guid:%d",guid),"gate",gate)
 			s.gate = gate
 			s.server = server
 		end
