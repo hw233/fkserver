3849b5a99aaacc89a21bf8ded21fd2fabcc19cd6
liligit2019 <liligit2019@gmail.com>
2022-08-18 10:24:26 +0800

    炸金花重连，玩家为空，不给重连

--- geek/script/game/zhajinhua/zhajinhua_table.lua
+++ geek/script/game/zhajinhua/zhajinhua_table.lua
@@ -2093,10 +2093,11 @@ function zhajinhua_table:can_sit_down(player,chair_id,reconnect)
 	if reconnect then 
 		if self.players[chair_id] then
 			log.info("reconnect player is exist guid:   %d chairid:   d%",player.guid,chair_id)
+			return enum.ERROR_NONE 
 		else
 			log.info("-------金花出现重入玩家-------reconnect player is not exist chairid:  %d      tableid:    %d",chair_id,self.table_id_)
+			return enum.ERROR_INTERNAL_UNKOWN
 		end
-		return enum.ERROR_NONE 
 	end
 
 	if self.players[chair_id] then
