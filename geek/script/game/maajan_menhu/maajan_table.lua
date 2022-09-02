31ef5da036c8725625c5a4f4ece401e418aba04d
lili <liligit2019@gmail.com>
2022-09-02 18:52:33 +0800

    联盟查询满桌，就是查询所有的桌子

--- geek/script/game/maajan_menhu/maajan_table.lua
+++ geek/script/game/maajan_menhu/maajan_table.lua
@@ -3019,10 +3019,10 @@ function maajan_table:global_status_info(type)
     if type then
         local n = table.nums(self.players)
 	    local min_count = self.start_count or self.room_.min_gamer_count or self.chair_count
-        if type == 1 then -- 查询满人桌子
-            if n < min_count then -- 过滤等待中的桌子
-                return
-            end
+        if type == 1 then -- 查询全部桌子
+            -- if n < min_count then -- 过滤等待中的桌子
+            --     return
+            -- end
         elseif type == 2 then -- 查询等待中的桌子
             if n >= min_count then -- 过滤满人的桌子
                 return
